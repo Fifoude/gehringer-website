6dda9c6383cf9e4ae7f9562f1ab03f084f05d7cf
pgehri <127775037+pgehri@users.noreply.github.com>
2025-11-08 11:18:09 +0100

    testons le formulaire contact

---
+++ src/config/seo.ts
@@ -0,0 +1,10 @@
+// src/config/seo.ts
+export const defaultSEO = {
+  siteName: "Gehringer Conseil",
+  siteUrl: "https://www.gehringer.fr",
+  title: "Gehringer Conseil – Management de transition & décarbonation",
+  description:
+    "Philippe Gehringer accompagne les entreprises dans leur transformation stratégique, écologique et énergétique. Manager de transition certifié Bilan Carbone®.",
+  author: "Philippe Gehringer",
+  image: "/images/og-default.jpg", // tu peux mettre ton logo ou une image de présentation
+};
